b2ef981769e6e28b060e4d8bab89905ce8438723
erickdc0102 <126851346+erickdc0102@users.noreply.github.com>
2023-05-26 13:16:35 -0500

    Lo primero

---
+++ src/Principal.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Erick DC
+ */
+public class Principal {
+    public static void main(String [] args){
+    System.out.print("La lista esta vacia ");
+    }
+}
