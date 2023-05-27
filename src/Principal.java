f70ced99de3f0d58c4475ebeb86868e4fe218718
erickdc0102 <126851346+erickdc0102@users.noreply.github.com>
2023-05-26 19:44:47 -0500

    menu

--- src/Principal.java
+++ src/Principal.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -9,6 +12,48 @@
  */
 public class Principal {
     public static void main(String [] args){
-    System.out.print("La lista esta vacia ");
+        int opc;
+    Scanner sc = new Scanner(System.in);
+    boolean salir = false;
+        while (!salir) {
+            System.out.println("Proyecto Final Estructuras");
+            System.out.println("Temas del semestre");
+            System.out.println("Eliga un tema");
+            System.out.println("1. Recursividad");
+            System.out.println("2. Metodos de busqueda");
+            System.out.println("3. Metodos de ordenamiento");
+            System.out.println("4. Estructuras lineales");
+            System.out.println("5. Grafos");
+            System.out.println("6. Salir");
+            opc = sc.nextInt();
+            switch (opc) {
+                case 1:
+                    break;
+                case 2:
+                    boolean sal = false;
+                    while (!sal) {
+                        System.out.println("Metodos de busqueda");
+                        System.out.println("Eliga un tema");
+                        System.out.println("1. Busqueda Secuencial");
+                        System.out.println("2. Busqueda Binaria");
+                        opc = sc.nextInt();
+                        switch (opc) {
+                            case 1:
+                                break;
+                            case 2:
+                                break;
+                        }
+                    }
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+            }
+        }
     }
 }
